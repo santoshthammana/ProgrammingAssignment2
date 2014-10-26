1f3300fe8b63f47181d8801da03d988bf08b4b94
SThammana <sthammana@C02N62A6G3QD.local>
2014-10-26 14:30:39 -0400

    Minor changes to comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,40 @@
-# Matrix inversion can be time intensive for a large matrix.  The functions below allow the results of matrix inversion to be cached for quick retrieval.
+# Matrix inversion can be time intensive for a large matrix.
+# The functions below allow the results of matrix inversion to be cached for quick retrieval.
 
-## makeCacheMatrix takes in originalMatrix as input and returns a "cache matrix", which is a list of functions allowing for a matrix inverse to be cached.
+## makeCacheMatrix takes in originalMatrix as input and returns a "cache matrix",
+## which is a list of functions allowing for a matrix inverse to be cached.
 
 makeCacheMatrix <- function(originalMatrix = matrix()) {
 
-  # set inverseMatrix to NULL, because we haven't calculated the inverse yet
+  ## set inverseMatrix to NULL, because we haven't calculated the inverse yet
   inverseMatrix <- NULL
 
-  # setMatrix sets the matrix to be inverted
+  ## setMatrix sets the matrix to be inverted
   setMatrix <- function(mat) {
     originalMatrix <<- mat
-    # inverseMatrix is set to NULL because the inverse of this new matrix needs to be computed.
-    # This will remove results from any inverse calculations of a previous matrix.
+    ## inverseMatrix is set to NULL because the inverse of this new matrix needs to be computed.
+    ## This will remove results from any inverse calculations of a previous matrix.
     inverseMatrix <<- NULL
   }
 
-  # getMatrix returns the matrix to be inverted
+  ## getMatrix returns the matrix to be inverted
   getMatrix <- function() originalMatrix
 
-  # setInverse sets the matrix inverse
+  ## setInverse sets the matrix inverse
   setInverse <- function(inv) inverseMatrix <<- inv
 
-  # getInverse returns the inverse set by setInverse
+  ## getInverse returns the inverse set by setInverse
   getInverse <- function() inverseMatrix
 
-  # return the cache matrix, which is a list of the above functions.
+  ## return the cache matrix, which is a list of the above functions.
   list(setMatrix = setMatrix, getMatrix = getMatrix,
        setInverse = setInverse,
        getInverse = getInverse)
 }
 
 
-## cacheSolve returns the inverse of the matrix stored in cacheMatrix.  If cached, the inverse is simply retrieved.  Otherwise, the inverse is calculated and cached.
+## cacheSolve returns the inverse of the matrix stored in cacheMatrix.
+## If cached, the inverse is simply retrieved.  Otherwise, the inverse is calculated and cached.
 ## Additional parameters to the solve function can be passed here.
 
 cacheSolve <- function(cacheMatrix, ...) {
@@ -51,7 +54,9 @@ cacheSolve <- function(cacheMatrix, ...) {
   ## Compute the matrix inverse.
   inv <- solve(data, ...)
 
-  ## Cache the matrix inverse in cacheMatrix.  If the original matrix remains the same, then the next time this function is called, the inverse will be retrieved from the cache.
+  ## Cache the matrix inverse in cacheMatrix.
+  ## If the original matrix remains the same, then the next time this function is called,
+  ## the inverse will be retrieved from the cache.
   cacheMatrix$setInverse(inv)
   inv
 }
